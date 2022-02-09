05fe2dbb2f93fa4086c7d5b5992e76e329bc5772
mrjohnbeard <wengerbeard@gmail.com>
2022-02-09 04:00:12 -0800

    cleanup pt1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/App/App';
 import { BrowserRouter as Router} from 'react-router-dom';
-// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
